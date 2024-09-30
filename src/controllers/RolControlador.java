785c94b9866e0e702769c5816002c164a1a48b1c
Ever VC <ever.vasquezc27@gmail.com>
2024-09-29 22:13:14 -0600

    🚧 Work in Progress:  Obtener rol por id

---
+++ src/controllers/RolControlador.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import connection.Conexion;
+import javax.persistence.EntityManager;
+import models.Rol;
+
+/**
+ *
+ * @author Ever Vásquez
+ */
+public class RolControlador {
+    private static RolControlador _instancia;
+    private EntityManager _entityManager;
+    
+    /***
+     * Permite acceder de forma estática a los métodos de la clase.
+     * @return _instancia
+    */
+    public static RolControlador Instancia() {
+        if (_instancia == null) {
+            _instancia = new RolControlador();
+        }
+        
+        return _instancia;
+    }
+    
+    private EntityManager setEntityManager() {
+        return Conexion.Instancia().getFabrica().createEntityManager();
+    }
+    
+    public Rol getRolPorId(Integer id) {
+        _entityManager = setEntityManager();
+        _entityManager.getTransaction().begin();
+        Rol rolBuscado = _entityManager.find(Rol.class, id);
+        _entityManager.close();
+        return rolBuscado;
+    }
+}
