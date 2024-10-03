be192c23d81cbedd585dc48cb7da6e5dbbac7c53
Ever VC <ever.vasquezc27@gmail.com>
2024-10-02 20:20:14 -0600

    ✨ New Feature: Controlador con crud de empresas

---
+++ src/controllers/EmpresaControlador.java
@@ -0,0 +1,58 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controllers;
+
+import connection.Conexion;
+import java.util.List;
+import javax.persistence.EntityManager;
+import models.Empresa;
+
+/**
+ *
+ * @author Ever Vásquez
+ */
+public class EmpresaControlador {
+    private static EmpresaControlador _instancia;
+    private EntityManager _entityManager;
+    
+    public static EmpresaControlador Instancia() {
+        if (_instancia == null) {
+            _instancia = new EmpresaControlador();
+        }
+        return _instancia;
+    }
+    
+    private EntityManager setEntityManager() {
+        return Conexion.Instancia().getFabrica().createEntityManager();
+    }
+    
+    public void CrearEmpresa(Empresa nuevaEmpresa) {
+        _entityManager = setEntityManager();
+        _entityManager.getTransaction().begin();
+        _entityManager.persist(nuevaEmpresa);
+        _entityManager.getTransaction().commit();
+        _entityManager.close();
+    }
+    
+    public Empresa GetEmpresaPorId(Integer id) {
+        _entityManager = setEntityManager();
+        _entityManager.getTransaction().begin();
+        Empresa empresaBuscada = _entityManager.find(Empresa.class, id);
+        _entityManager.close();
+        return empresaBuscada;
+    }
+    
+    public List<Empresa> GetListaUsuarios() {
+        _entityManager = setEntityManager();
+        _entityManager.getTransaction().begin();
+        return _entityManager.createQuery("SELECT empresas FROM Empresa empresas").getResultList();
+    }
+    
+    public void ActualizarEmpresa(Empresa empresaActualizada) {
+        _entityManager = setEntityManager();
+        _entityManager.getTransaction().begin();
+        Empresa empresaAActualizar = _entityManager.find(Empresa.class, empresaActualizada.getId());
+    }
+}
